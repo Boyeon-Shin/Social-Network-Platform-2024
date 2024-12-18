4b195276c2bb4048c99bd91ebee067b44922d0bd
Boyeon-Shin <anna007096@gmail.com>
2024-12-18 19:42:06 +0900

    .

--- src/io/moblie/conf/Conf.java
+++ src/io/moblie/conf/Conf.java
@@ -1,7 +1,6 @@
 package io.moblie.conf;
 
 public interface Conf {
-    String DB_URL = "jdbc:postgresql://dbclass.wisoft.io:10008/pingmong";
-    String DB_USER = "pingmong";
-    String DB_PASSWORD = "kny7838";
 }
+
+
