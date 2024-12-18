6cbba332133c6331ee6bc367627b016619131ecb
Boyeon-Shin <anna007096@gmail.com>
2024-12-18 16:15:10 +0900

    UserClass 생성

---
+++ src/io/moblie/platform/users/UserService.java
@@ -0,0 +1,5 @@
+package io.moblie.platform.users;
+
+public class UserService {
+
+}
---
+++ src/io/moblie/platform/users/Users.java
@@ -0,0 +1,51 @@
+package io.moblie.platform.users;
+
+public class Users {
+    private String userId;
+    private String userName;
+    private String userEmail;
+    private String sex;
+    private String phoneNumber;
+
+    public Users(final String userId, final String userName, final String userEmail, final String sex,
+                 final String jobTitle, final String phoneNumber) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.sex = sex;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUseName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String PhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Users {" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userEmail=" + userEmail +
+                ", sex='" + sex + '\'' +
+                ", phoneNumber='" + phoneNumber +
+                '}';
+    }
+}
+
+
